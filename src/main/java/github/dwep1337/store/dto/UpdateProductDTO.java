57ae6aaf0c71e4d5e3a146a5bd63a8e92b84f54e
dwep1337 <gemer942@gmail.com>
2026-05-17 21:26:33 -0300

    Refactor UpdateProductDTO validation annotations

--- src/main/java/github/dwep1337/store/dto/UpdateProductDTO.java
+++ src/main/java/github/dwep1337/store/dto/UpdateProductDTO.java
@@ -4,16 +4,13 @@ import java.math.BigDecimal;
 
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record UpdateProductDTO(
-    @NotBlank(message = "Nome não pode ser vazio")
     @Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres")
     String name,
 
-    @NotBlank(message = "Descrição não pode ser vazia")
     @Size(max = 255, message = "Descrição deve ter no máximo 255 caracteres")
     String description,
 
